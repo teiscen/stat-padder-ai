8c39f4c79d7be2e84d90d2b4623e15424bf6acfa
teiscen <teiscen@gmail.com>
2025-10-02 18:05:26 -0700

    Refactoring

--- data_load.py
+++ data_load.py
@@ -1,84 +1,103 @@
 import pandas as pd
 import os
 
-"""
-Data Format:
-player_id, position, --injury_status--, team_id, 
-Minutes, FG, ThreePT, FT, REB, AST, BLK, STL, PF, TO, PTS
-"""
 STARTING_FOLDER         = os.path.join('.', 'Data', 'NBA')
 INJURIES_CSV_FILE       = os.path.join(STARTING_FOLDER, 'News',   'injuryReport.csv') 
 PLAYERS_CSV_FILE        = os.path.join(STARTING_FOLDER, 'Roster', 'players.csv')
 GAMES_CSV_FILE          = os.path.join(STARTING_FOLDER, 'Roster', 'gameCSV.csv')
 PARTICIPATED_CSV_FILE   = os.path.join(STARTING_FOLDER, 'Roster', 'participatedCSV.csv')
+DEBUG_PRINT = False
 
-# Load the data, and drop the columns that are unneeded 
-""" 
-Injury Columns : playerName, position, returnDate, status, comment
-Columns To Drop:             position,             status, 
-Notes:  Need to use this to constuct the game data with the appropriate status level.
-            i.e. Player was injured, or fine,
-        Need to parse out date from the comment to determine when the injury took place
-            create accurate labeling
-"""
-#TODO - Need to determine how the injury labelling will work. Discuss with Paul
-# injury_column_to_drop = ['position', 'status']
-# injuries_data = pd.read_csv(INJURIES_CSV_FILE)
-# injuries_data = injuries_data.drop(injury_column_to_drop)
+# Load Data
+def get_players_data():
+    """ Players Columns: position, playerID """
+    players_column_to_drop = ['playerName', 'playerURL', 'teamName', 'teamURL', 'teamID', 'status', 'src']
+    players_data = pd.read_csv(PLAYERS_CSV_FILE)
+    players_data = players_data.drop(players_column_to_drop, axis=1)#, errors='ignore')
+    
+    if(DEBUG_PRINT): print("Players Columns:", players_data.columns.tolist())
+    return players_data
 
-"""
-Players Columns: playerName, position, playerURL, teamName, teamURL, teamID, playerID, status, src
-Columns to Drop: playerName            playerURL, teamName, teamURL, teamID,           status, src
-"""
-players_column_to_drop = ['playerName', 'playerURL', 'teamName', 'teamURL', 'teamID', 'status', 'src']
-players_data = pd.read_csv(PLAYERS_CSV_FILE)
-# print("Players Columns:", players_data.columns.tolist())
-players_data = players_data.drop(players_column_to_drop, axis=1)#, errors='ignore')
+def get_participated_data():
+    """ Participated Columns: teamID, gameDate, Minutes, FG, ThreePT, FT, REB, AST, BLK, STL, PF, TO, PTS, playerID """
+    participated_data_columns_to_drop = ['FGPercent', 'ThreePTPercent', 'FTPercent']
+    participated_data = pd.read_csv(PARTICIPATED_CSV_FILE)
+    participated_data = participated_data.drop(participated_data_columns_to_drop, axis=1)#, errors='ignore' )
+    
+    if(DEBUG_PRINT): print("Participated columns:", participated_data.columns.tolist())
+    return participated_data
 
-"""
-Participated Columns: teamID, gameDate, Minutes, FG, FGPercent, ThreePT, ThreePTPercent, FT, FTPercent, REB, AST, BLK, STL, PF, TO, PTS, playerID
-Columns to Drop     :                                FGPercent,          ThreePTPercent,     FTPercent
-Notes:  Need to parse FG, ThreePT, FT, into _successes and _attempts for each
-"""
-participated_data_columns_to_drop = ['FGPercent', 'ThreePTPercent', 'FTPercent']
-participated_data = pd.read_csv(PARTICIPATED_CSV_FILE)
-# print("Participated columns:", participated_data.columns.tolist())
-participated_data = participated_data.drop(participated_data_columns_to_drop, axis=1)#, errors='ignore' )
+def get_games_data():
+    """ Games Columns: teamID, gameDate, awayTeamID """
+    games_data_columns_to_drop = ['result']
+    games_data = pd.read_csv(GAMES_CSV_FILE)
+    games_data = games_data.drop(games_data_columns_to_drop, axis=1)#, errors='ignore')
+    
+    if(DEBUG_PRINT): print("Games Columns:", games_data.columns.tolist())
+    return games_data
 
-"""
-Games Columns  : teamID, gameDate, awayTeamID, result
-Columns to Drop:                               result
-Notes:  Need to add a column indicating home or away
-"""
-games_data_columns_to_drop = ['result']
-games_data = pd.read_csv(GAMES_CSV_FILE)
-# print("Games Columns:", games_data.columns.tolist())
-games_data = games_data.drop(games_data_columns_to_drop, axis=1)#, errors='ignore')
-
-# Merge on the data sets
-"""
-How:
-    'left': All rows from the left DataFrame, matching rows from the right.
-    'right': All rows from the right DataFrame, matching rows from the left.
-    'inner': Only rows with keys in both DataFrames.
-    'outer': All rows from both DataFrames, fill missing with NaN.
-"""
-# teamID, gameDate, Minutes, FG, ThreePT, FT, REB, AST, BLK, STL, PF, TO, PTS, playerID, position
-players_participated_data = pd.merge(players_data, participated_data, on='playerID', how='inner')
-del players_data, participated_data
-
-# teamID, gameDate, Minutes, FG, ThreePT, FT, REB, AST, BLK, STL, PF, TO, PTS, playerID, position, awayTeamID, 
-merged_data = pd.merge(players_participated_data, games_data, on=['teamID', 'gameDate'], how='inner')
-del players_participated_data, games_data
-
-"""
-Days per last game
-"""
-# merged_data['gameDate'] = pd.to_datetime(merged_data['gameDate'])
-# merged_data = merged_data.sort_values(['playerID', 'gameDate'])
-# merged_data['days_since_last_game'] = merged_data.groupby('playerID')['gameDate'].diff().dt.days
-# merged_data['days_since_last_game'] = merged_data['days_since_last_game'].fillna(0)  # Or another default for first game
-
-# To return dataset
+# Merge Data
 def get_merged_data():
+    """ 
+    Merged Columns: 
+    playerID, position, gameDate, teamID, awayTeamID, 
+    Minutes, FG, ThreePT, FT, REB, AST, BLK, STL, PF, TO, PTS,  
+    """
+    player_data = get_players_data()
+    participated_data = get_participated_data()
+    games_data = get_games_data()
+    merged_data = pd.merge(
+                    pd.merge(player_data, participated_data, on='playerID', how='inner'), 
+                    games_data, on=['teamID', 'gameDate'], how='inner')
+
+    del player_data, participated_data, games_data
+    if(DEBUG_PRINT): print("Merged Columns: ", merged_data.columns.tolist())
     return merged_data
+    
+# Split the data
+def split_baskets(nba_data):
+    """ FG, FT, ThreePT -> X_successes, X_attempts """
+    columns_to_split = ['FG', 'FT', 'ThreePT']
+    for column in columns_to_split:
+        # assign the create columns to                          Split column into 2 seperate columns based on - as type int
+        nba_data[[f'{column}_successes', f'{column}_attempts']] = nba_data[column].str.split('-', expand=True).astype(int)
+        nba_data.drop([column], axis=1, inplace=True)
+
+    if(DEBUG_PRINT): print("Split Baskets Columns: ", nba_data.columns.tolist())
+
+def split_awayTeamID(nba_data):
+    """ awayTeamID -> oppTeamID, isHome """
+    nba_data['isHome'] = (~nba_data['awayTeamID'].str.startswith('@')).astype(int)
+    nba_data['awayTeamID'] = (
+        nba_data['awayTeamID']
+        .str.replace('@', '', regex=False)
+        .str.replace('vs','', regex=False)
+        .str.strip() 
+        .str.lower()
+    )
+
+    if(DEBUG_PRINT): print("Split AwayTeam Columns: ", nba_data.columns.tolist())
+
+# Add a score
+# TODO: Find a better way of handling formulas
+def add_label(nba_data, formula=None):
+    if formula is None:
+        formula = lambda ThreePT, FG, FT, REB, AST, BLK, STL, TO: (
+            (ThreePT * 3) + (FG * 2) + (FT * 1) + (REB * 1.2) + (AST * 1.5) + (BLK * 2) + (STL * 2) + (TO * -1)
+        )
+
+    """ Formula implements default point scoring. Can be customized. """
+    nba_data['fantasy_points'] = nba_data.apply(
+        lambda row: formula(row['ThreePT_successes'], row['FG_successes'], row['FT_successes'], 
+                            row['REB'], row['AST'], row['BLK'], row['STL'], row['TO']),
+        axis=1
+    )
+
+def get_nba_data(formula=None):
+    formula = None # TODO: Get rid of when a solution for labelling is found
+    
+    nba_data = get_merged_data()
+    split_baskets(nba_data)
+    split_awayTeamID(nba_data)
+    add_label(nba_data, formula)  # Uses default formula
+    return nba_data
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,139 +1,63 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-import data_load
 import numpy as np
 
+def create_embedded_columns(nba_data):
+    """ returns the input dims of the embedded columns"""
+    embedded_columns = ['playerID', 'teamID', 'awayTeamID', 'position']
+    for col in embedded_columns:
+        nba_data[col] = nba_data[col].astype('category').cat.codes
 
-""" teamID, gameDate, Minutes, FG, ThreePT, FT, REB, AST, BLK, STL, PF, TO, PTS, playerID, position, awayTeamID, """
-nba_data = data_load.get_merged_data()
-# print("NBA Data Colums: ", nba_data.columns.tolist())
-
-""" ..., FG_successes, GF_attempts, ThreePT_successes, ThreePT_attempts, FT_successes, FT_attempts, ... """
-# split FG, ThreePT, FT -> _successes and _attempts
-columns_to_split = ['FG', 'FT', 'ThreePT']
-for column in columns_to_split:
-    # assign the create columns to                          Split column into 2 seperate columns based on - as type int
-    nba_data[[f'{column}_successes', f'{column}_attempts']] = nba_data[column].str.split('-', expand=True).astype(int)
-    nba_data.drop([column], axis=1, inplace=True)
-
-""" ..., awayTeamID, isHome """
-# split awayTeamID -> oppTeamID and isHome 
-nba_data['isHome'] = (~nba_data['awayTeamID'].str.startswith('@')).astype(int)
-nba_data['awayTeamID'] = (
-    nba_data['awayTeamID']
-    .str.replace('@', '', regex=False)
-    .str.replace('vs','', regex=False)
-    .str.strip() 
-    .str.lower()
-)
-
-# Encode the features ... 
-"""
-    playerID,          int,     embedding     
-    position,          int,     embedding   
-    gameDate,          ---,     Drop     
-    teamID,            int,     embedding
-    awayTeamID,        int,     embedding         
-    isHome             bool,    Binary 
-    Minutes,           int,     Normalize    
-    FG_successes,      int,     Normalize        
-    FG_attempts,       int,     Normalize        
-    ThreePT_successes, int,     Normalize            
-    ThreePT_attempts,  int,     Normalize            
-    FT_successes,      int,     Normalize        
-    FT_attempts        int,     Normalize        
-    REB,               int,     Normalize
-    AST,               int,     Normalize
-    BLK,               int,     Normalize
-    STL,               int,     Normalize
-    PF,                int,     Normalize
-    TO,                int,     Normalize
-    PTS,               int,     Normalize
-"""
-# One-Hot Encoding
-# one_hot_columns = ['position']
-# nba_data = pd.get_dummies(nba_data, columns=one_hot_columns)
-
-# Embeddings
-embedded_columns = ['playerID', 'teamID', 'awayTeamID', 'position']
-for col in embedded_columns:
-    nba_data[col] = nba_data[col].astype('category').cat.codes
-embedding_input_dims = {col: nba_data[col].nunique() for col in embedded_columns}
+    return {col: nba_data[col].nunique() for col in embedded_columns}
 
 # Standardizing
-standardized_columns = [
-    'Minutes', 'FG_successes', 'FG_attempts', 'ThreePT_successes', 'ThreePT_attempts',
-    'FT_successes', 'FT_attempts', 'REB', 'AST', 'BLK', 'STL', 'PF', 'TO', 'PTS'
-]
-scaler = StandardScaler()
-nba_data[standardized_columns] = scaler.fit_transform(nba_data[standardized_columns])
+def create_standardized_columns(nba_data):
+    standardized_columns = [
+        'Minutes', 'FG_successes', 'FG_attempts', 'ThreePT_successes', 'ThreePT_attempts',
+        'FT_successes', 'FT_attempts', 'REB', 'AST', 'BLK', 'STL', 'PF', 'TO', 'PTS'
+    ]
+    scaler = StandardScaler()
+    nba_data[standardized_columns] = scaler.fit_transform(nba_data[standardized_columns])
 
-# Labeling
-def formula(ThreePT, FG, FT, REB, AST, BLK, STL, TO):
-    return (ThreePT*3) + (FG*2) + (FT*1) + (REB*1.2) + (AST*1.5) + (BLK*2) + (STL*2) + (TO*-1)
-
-nba_data['fantasy_points'] = nba_data.apply(
-    lambda row: formula( row['ThreePT_successes'], row['FG_successes'], row['FT_successes'], 
-                        row['REB'], row['AST'], row['BLK'], row['STL'], row['TO']),
-    axis=1
-)
 
 # Create the Sequences
-"""
-playerID, position,
-    [... game 1 ...], 
-    ...,
-    [... game SEQUENCE_LENGTH ...]
-"""
-SEQUENCE_LENGTH = 20
-nba_data = nba_data.groupby('playerID').filter(lambda g: len(g) > SEQUENCE_LENGTH + 1) # Ignore players with less than the sequence length
+def generate_sequences(nba_data, sequence_length=20): 
+    # Ignore players with less than the sequence length
+    nba_data = nba_data.groupby('playerID').filter(lambda g: len(g) > sequence_length + 1) 
 
-sequences = []
-labels = []
+    featuresList = [
+        'playerID', 'position', 'teamID', 'awayTeamID', 'isHome', 'Minutes',
+        'FG_successes', 'FG_attempts', 'ThreePT_successes', 'ThreePT_attempts', 
+        'FT_successes', 'FT_attempts','REB', 'AST', 'BLK', 'STL', 'PF', 'TO', 'PTS',
+    ]
+    sequences    = []
+    labels       = []
 
-featuresList = [
-    'playerID', 'position',
-    'teamID', 'awayTeamID', 'isHome', 'Minutes',
-    'FG_successes', 'FG_attempts', 'ThreePT_successes', 'ThreePT_attempts', 'FT_successes', 'FT_attempts',
-    'REB', 'AST', 'BLK', 'STL', 'PF', 'TO', 'PTS',
-]
+    for playerID, group in nba_data.groupby('playerID'):
+        group = group.sort_values('gameDate')
+        for i in range(len(group) - sequence_length):
+            seq = group[featuresList].iloc[i:i+sequence_length].values
+            label = group['fantasy_points'].iloc[i+sequence_length]
+            sequences.append(seq)
+            labels.append(label)
 
-for playerID, group in nba_data.groupby('playerID'):
-    group = group.sort_values('gameDate')
-    for i in range(len(group) - SEQUENCE_LENGTH):
-        seq = group[featuresList].iloc[i:i+SEQUENCE_LENGTH].values
-        label = group['fantasy_points'].iloc[i+SEQUENCE_LENGTH]
-        sequences.append(seq)
-        labels.append(label)
+    return sequences, labels
 
-# testing
-# print(f"Number of sequences: {len(sequences)}")
-# print(f"Shape of one sequence: {sequences[0].shape}")  # Should be (SEQUENCE_LENGTH, number_of_features)
-# print(f"Type of sequences: {type(sequences)}")
+# # Convert to arrays
+# sequences_array = np.array(sequences)
+# labels_array = np.array(labels)
 
-# print("First sequence:\n", np.array(sequences[0]))
-# print("First label:", labels[0])
+# sequences_array[:, :, 0] = sequences_array[:, :, 0].astype('int32')  # playerID
+# sequences_array[:, :, 1] = sequences_array[:, :, 1].astype('int32')  # position
+# sequences_array[:, :, 2] = sequences_array[:, :, 2].astype('int32')  # teamID
+# sequences_array[:, :, 3] = sequences_array[:, :, 3].astype('int32')  # awayTeamID
 
-# Added code block
-# player_games = nba_data.groupby('playerID').filter(lambda g: len(g) > SEQUENCE_LENGTH)
-# print("Expected label:", player_games['fantasy_points'].iloc[SEQUENCE_LENGTH])
-# print("Actual label:", labels[0])
+# print("Example sequence:\n", sequences_array[0])
 
-# Convert to arrays
-sequences_array = np.array(sequences)
-labels_array = np.array(labels)
+# print("isHome dtype in sequences_array:", sequences_array[:, :, 4].dtype)
+# print("Unique values in isHome:", np.unique(sequences_array[:, :, 4]))
 
-sequences_array[:, :, 0] = sequences_array[:, :, 0].astype('int32')  # playerID
-sequences_array[:, :, 1] = sequences_array[:, :, 1].astype('int32')  # position
-sequences_array[:, :, 2] = sequences_array[:, :, 2].astype('int32')  # teamID
-sequences_array[:, :, 3] = sequences_array[:, :, 3].astype('int32')  # awayTeamID
-
-print("Example sequence:\n", sequences_array[0])
-
-print("isHome dtype in sequences_array:", sequences_array[:, :, 4].dtype)
-print("Unique values in isHome:", np.unique(sequences_array[:, :, 4]))
-
-# Save to disk
-np.save('Data/Formatted/sequences.npy', sequences_array)
-np.save('Data/Formatted/labels.npy', labels_array)
-np.save('Data/Formatted/embedding_input_dims.npy', embedding_input_dims)
+# # Save to disk
+# np.save('Data/Formatted/sequences.npy', sequences_array)
+# np.save('Data/Formatted/labels.npy', labels_array)
+# np.save('Data/Formatted/embedding_input_dims.npy', embedding_input_dims)
